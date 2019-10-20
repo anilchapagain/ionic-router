05216b772443560eaac05d18a47214eedfff4125
anilchapagain68 <anilchapagain68@gmail.com>
2019-10-20 22:40:35 +0530

    removed comments

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -9,10 +9,6 @@ import { HomePage } from './home.page';
 import { CustomModule } from '../Module/custom/custom.module';
 import { HomeRoutingModule } from './home-routing.module';
 
-// import { AComponent } from '../components/a/a.component';
-// import { BComponent } from '../components/b/b.component';
-// import { CComponent } from '../components/c/c.component';
-
 const routes: Routes = [
   {
     path: '',
